26fb12d6b051f35d94f405f2d5406a00e2f8271d
AKiLadian <akiladian.325.shopping@gmail.com>
2022-07-13 18:09:27 +0900

    HoT: initial revision

--- Ex4/Heads_or_Tail.c
+++ Ex4/Heads_or_Tail.c
@@ -2,9 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define heads 1
+#define tails 2
+
 int main ()
 {
-    int a;
+    int times;
+    char isheads[6] = "Heads";
+    char istails[6] = "Tails";
+
+    printf("Tossing a coin...\n");
+    srand(time(NULL));
+
+    for(times = 0 ; times < 3 ; times++)
+    {
+        printf("Round %d: ", times + 1);
+
+        if((rand() % 2) + 1 == 1) printf("%s\n", isheads);
+        else printf("%s\n", istails);
+
+    }
+
+    printf("Heads: %d, Tails: %d\n", heads, tails);
 
     return 0;
 }
